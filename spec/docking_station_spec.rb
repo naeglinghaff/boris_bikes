67f29bbe65bf47887dc0a6949b0920e05926d94f
lilawalker <lilawlker@gmail.com>
2020-02-19 18:22:54 +0000

    Add rspec test for release bike error message

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -3,11 +3,6 @@ require "docking_station"
 describe DockingStation do
 	it { is_expected.to respond_to(:release_bike) }
 
-	it 'releases working bikes' do
-		bike = subject.release_bike
-		expect(bike).to be_working
-	end
-
 	it { is_expected.to respond_to(:dock).with(1).argument }
 
 	it 'docks bikes' do
@@ -15,4 +10,18 @@ describe DockingStation do
 		subject.dock(bike)
 		expect(subject.dock(bike)).to eq bike
 	end
+
+	describe '#release_bike' do
+		it 'releases a bike' do
+			bike = Bike.new
+			expect(bike).to be_working
+			subject.dock(bike)
+			expect(subject.release_bike).to eq bike
+		end
+
+		it 'raises an error when there are no bikes available' do
+			expect {subject.release_bike}.to raise_error("No bikes available")
+		end
+	end
+
 end
